34292c069e897ac815c228b6c6e3f855af21b921
jbonn360 <justinbonnici94@gmail.com>
2022-06-19 22:48:40 +0200

    Add third project

---
+++ 3. spring-boot-microservices-config-server/spring-cloud-config-server/src/test/java/io/jbonn360/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jbonn360.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
